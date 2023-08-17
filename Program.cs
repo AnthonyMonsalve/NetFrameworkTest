8b8d741f1ee45d90103c2784543eb43eee5314dc
Anthony Monsalve <35313563+AnthonyMonsalve@users.noreply.github.com>
2023-08-17 22:56:55 +0200

    CHAO

--- Program.cs
+++ Program.cs
@@ -9,22 +9,7 @@ namespace NetFrameworkTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("INSTAPAGO TEST FINAL con msbuild 2INSTAPAGO TEST FINAL con msbuild 2");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
+            Console.WriteLine("INSTAPAGO TEST FINAL con msbuild 2INCHAOOOSTAPAGO TEST FINAL con msbuild 2");
             Console.ReadLine();
         }
     }
