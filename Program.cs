05eed8f00d36a15c1fb92cc132e1799a26728962
Anthony Monsalve <35313563+AnthonyMonsalve@users.noreply.github.com>
2023-08-17 20:39:30 +0200

    asdasdaD

--- Program.cs
+++ Program.cs
@@ -10,20 +10,17 @@ namespace NetFrameworkTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
+            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");            
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-
+            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");            
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
 
+            Console.WriteLine("Hola Mundo Test de Compilacion GitHub")
+            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");            
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
-            
-            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
             
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
