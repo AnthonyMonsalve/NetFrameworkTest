7baf55330dc21eb4a1b25d49d48fe6b97d13a87c
Anthony Monsalve <35313563+AnthonyMonsalve@users.noreply.github.com>
2023-08-17 15:37:27 +0200

    txt count lines

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ namespace NetFrameworkTest
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
             Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
+            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
+            Console.WriteLine("Hola Mundo Test de Compilacion GitHub");
         }
     }
 }
